a57b24e35bbacf7a1e104f4337b40f68e528edbc
zhw <510666592@qq.com>
2018-10-29 18:22:40 +0800

    新建测试

---
+++ ccs-controller/src/main/java/team/yingyingmonster/ccs/controller/action/test.java
@@ -0,0 +1,11 @@
+package team.yingyingmonster.ccs.controller.action;
+
+/**
+ * @author Juer Whang <br/>
+ * - project: CompanyCheckSystem
+ * - create: 18:18 2018/10/29
+ * - sssssssss
+ **/
+
+public class test {
+}
