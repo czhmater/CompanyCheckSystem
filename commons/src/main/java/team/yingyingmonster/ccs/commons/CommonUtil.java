53c19d2d38c5a16b0486f328a49009d9b331ebfb
Juer Genie <JuerGenie@qq.com>
2018-10-30 09:58:02 +0800

    add angularjs to lib.

--- commons/src/main/java/team/yingyingmonster/ccs/commons/CommonUtil.java
+++ commons/src/main/java/team/yingyingmonster/ccs/commons/CommonUtil.java
@@ -1,10 +1,24 @@
 package team.yingyingmonster.ccs.commons;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import team.yingyingmonster.ccs.commons.log4j2.JuerDataSource;
+
 /**
  * @author Juer Whang <br/>
  * - project: CompanyCheckSystem
  * - create: 14:31 2018/10/29
  * -
  **/
+@ContextConfiguration("classpath:spring-common.xml")
+@Transactional("transactionManager")
 public class CommonUtil {
+    public static void main(String[] args) {
+        // TODO: something...
+//        JuerDataSource.init();
+        Logger logger = LogManager.getLogger(CommonUtil.class);
+        logger.debug("测试, " + logger.getClass());
+    }
 }
