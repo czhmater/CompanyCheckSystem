9ecae5b579f88529df632389302fcc01deb84c72
zhw <510666592@qq.com>
2018-10-31 17:42:09 +0800

    1、创建总检处理类 2、修改总检页面

---
+++ ccs-controller/src/main/java/team/yingyingmonster/ccs/controller/action/TotalInspectionAction.java
@@ -0,0 +1,24 @@
+package team.yingyingmonster.ccs.controller.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Zhao Han Wei <br/>
+ * - project: CompanyCheckSystem
+ * - create: 17:37 2018/10/31
+ * - 总检
+ **/
+
+@Controller
+
+@RequestMapping("/total-inspection")
+
+public class TotalInspectionAction {
+
+    @RequestMapping("/write")
+    public String writeTotalInspection(){
+        return "";
+    }
+
+}
